c69c2e6799f88971dae72c2f723e3617709b330c
Taylor Savage <taylorsavage93@gmail.com>
2022-08-11 15:25:54 -0400

    finished exercise view product

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -21,6 +21,12 @@ namespace Testing.Controllers
             var products = repo.GetAllProducts();
             return View(products);
         }
+
+        public IActionResult ViewProduct(int id)
+        {
+            var product = repo.GetProduct(id);
+            return View(product);
+        }
       
     }
 }
